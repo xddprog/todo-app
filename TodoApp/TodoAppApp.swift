ec62b102e5b631e661fdb82219804fc1eb7dc14a
mago <magomedovmarif2@gmail.com>
2024-11-10 23:37:33 +0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by mago on 10.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
